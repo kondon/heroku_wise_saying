2e48d86bb0d69526bf6916bb21266bcdf9f151a0
Kondo <shinji_kondo@post.pioneer.co.jp>
2015-12-22 19:46:37 +0900

    Update時にupdates.jsにアクセスするfunction update() add

--- web/js/function.js
+++ web/js/function.js
@@ -1,11 +1,12 @@
+//挿入のJs
 function insert(){
 	/*
 	console.log($('#name').attr('class'));
-	console.log($('#meigen').attr('class'));	
+	console.log($('#meigen').attr('class'));
 	*/
 	console.log($('#name').val());
 	console.log($('#meigen').val());
-	
+
 	if($('#name').val() == ""){
 		alert("名前が入ってません!!");
 		return;
@@ -14,7 +15,7 @@ function insert(){
 		alert("名言が入ってません!!");
 		return;
 	}
-	
+
 	$.ajax({
 			//url:"./php/sli_get_image_circle.php",		//spiral用
 			url :"./insert.php",
@@ -26,11 +27,11 @@ function insert(){
 				//$('#sli_gazou').attr('src','./image/'+Date_time+'/'+ImageID+'.jpg');
 				//$('#popup').attr('src','./image/'+Date_time+'/'+ImageID+'.jpg');
 				console.log(data);
-				
+
 				if(data == "success"){
 					alert("登録完了!!");
 					$('#name').val('');
-					$('#meigen').val('');	
+					$('#meigen').val('');
 				}else if(data == "exist"){
 					alert("その名言は既に登録されています。");
 				}else{
@@ -41,8 +42,37 @@ function insert(){
 				alert("update failed [network error]");
 			}
 		});
-	
-} 
+
+}
+
+function update(){
+
+	id = 101;
+
+	$.ajax({
+			//url:"./php/sli_get_image_circle.php",		//spiral用
+			url :"./updates.php",
+			type:"POST",
+			data:{"id": id},
+			async:true,
+			success:function(data){
+				//$('#sli_gazou').attr('src','./image/'+Date_time+'/'+ImageID+'.jpg');
+				//$('#popup').attr('src','./image/'+Date_time+'/'+ImageID+'.jpg');
+				console.log(data);
+
+				if(data == "success"){
+					alert("登録完了!!");
+				}else if(data == "exist"){
+					alert("その名言は既に登録されています。");
+				}else{
+					alert("その他エラーです。")
+				}
+			},
+			error:function(){
+				alert("update failed [network error]");
+			}
+		});
+}
 
 $("#view").on("click", function() {
   	select_view();
@@ -50,10 +80,7 @@ $("#view").on("click", function() {
   });
 
 var select_view = function(){
-	
+
 	console.log($('#view').attr('class'));
 	console.log('aaa');
 }
-
-
-
