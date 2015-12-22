9f88b8a2049180ddd9d13b867da037c678b4803f
Kondo <shinji_kondo@post.pioneer.co.jp>
2015-12-22 19:39:56 +0900

    Update分を実行。

--- web/updates.php
+++ web/updates.php
@@ -6,8 +6,10 @@ include('./DB_config.php');
 //$url = parse_url(getenv('DATABASE_URL'));
 
 //リクエストの取得
-$name = $_POST['name'];
-$meigen = $_POST['meigen'];
+$id = $_POST['id'];
+//$meigen = $_POST['meigen'];
+
+$meigen = "私の名前は";
 
 //DBに接続
 //local用
@@ -25,71 +27,20 @@ $result = pg_query(
 				);
 */
 
-//SELECT文
+//UPDate文
 //$test = 'ギタ・ベリン';
 //クエリ-の実行
 $result = pg_query(
 //				"CREATE TABLE kondo_test_auau(id integer,name text,password text);"
 //				"SELECT name,meigen FROM kondo_local where name = $name;"
-				"SELECT name,meigen FROM kondo_local where name = '$name';"
+//				"SELECT name,meigen FROM kondo_local where name = '$name';"
+        "UPDATE kondo_local SET meigen='$meigen' where id = '$id';"
 				);
-
-//すでに同じものがあればflagを立てる
-$flag=0;
-
-//echo pg_num_rows($result);
-
-if(pg_num_rows($result) != 0){
-	for ($i = 0 ; $i < pg_num_rows($result) ; $i++){
-	    $rows = pg_fetch_array($result, NULL, PGSQL_ASSOC);
-	    //$name = $rows['name'];
-	    //$meigen = $rows['meigen'];
-	    //DBから引っ張ってきた名言を整形
-	    $query_meigen = $rows['meigen'];
-	    $query_meigen1 = str_replace('。', '',$query_meigen);
-	    $query_meigen2 = str_replace('、', '',$query_meigen1);
-		/*
-		echo $query_meigen2;
-		echo strlen($query_meigen2);
-		echo mb_strlen($query_meigen2);
-		*/
-		//入力された名言を整形
-		$meigen_tmp1 = str_replace('。', '',$meigen);
-		$meigen_tmp2 = str_replace('、', '',$meigen_tmp1);
-		/*
-		echo 'aaaaa';
-		echo strlen($meigen_tmp2);
-		echo mb_strlen($meigen_tmp2);
-		*/
-		//echo mb_strlen($query_meigen2);
-		//echo 'aaaaa';
-		//echo mb_strlen($meigen_tmp2);
-		if(mb_strlen($query_meigen2) == mb_strlen($meigen_tmp2)){
-			$flag=1;
-			break;
-		}
-	}
+if (!$result) {
+  //失敗した際にseqの値を戻す必要もありそうだが。。。？
+    die('クエリーが失敗しました。'.pg_last_error());
 }
-
-//flagが1ならばDB内に、入力した名言と同じものがある(文字数が一緒なだけ。)
-if($flag == 1){
-	echo 'exist';
-}else{
-	//DBにないので、Insertする。
-	$insert_result = pg_query(
-//				"CREATE TABLE kondo_test_auau(id integer,name text,password text);"
-//				"SELECT name,meigen FROM kondo_local where name = $name;"
-				"insert into kondo_local (id, name, meigen)
-					values (nextval('id_seq'), '$name', '$meigen');"
-				);
-	if (!$insert_result) {
-		//失敗した際にseqの値を戻す必要もありそうだが。。。？
-	    die('クエリーが失敗しました。'.pg_last_error());
-	}
-	echo ('success');
-}
-
-
+echo ('success');
 /*
 if (!$result) {
     die('クエリーが失敗しました。'.pg_last_error());
